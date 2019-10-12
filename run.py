4b7058b6365b079736d343c0f5a6dac2a32cd521
Chris Acheson <mail@chrisacheson.net>
2019-10-12 14:58:32 -0400

    Initial GUI code
    Start of GUI main window and menu bar. GUI handles creation and opening
    of databases, instead of doing it in run.py with a command line
    argument.

--- run.py
+++ run.py
@@ -1,21 +1,6 @@
 #!/usr/bin/env python3
-import sys
-
-import rksmanager.database
+import rksmanager.gui
 
 
-# TODO: This file should just start the UI, which will handle opening/creating
-# the database and schema version checking/updating
-if len(sys.argv) < 2:
-    print("Usage: run.py <database filename>")
-    sys.exit()
-db = rksmanager.database.Database(sys.argv[1])
-schema_version = db.get_schema_version()
-if schema_version < db.expected_schema_version:
-    print("Applying database schema migrations")
-    db.apply_migrations()
-elif schema_version > db.expected_schema_version:
-    print("Database was created with a newer version of this program, please"
-          " upgrade")
-    sys.exit()
-print("Database is at schema version {}".format(db.get_schema_version()))
+gui = rksmanager.gui.Gui()
+gui.start()
