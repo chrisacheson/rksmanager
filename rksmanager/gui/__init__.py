c79b2cab5b75cd8e07aff032bf6070c3873ea608
Chris Acheson <mail@chrisacheson.net>
2019-11-03 04:22:42 -0500

    Editor widget for creating new person records

--- rksmanager/gui/__init__.py
+++ rksmanager/gui/__init__.py
@@ -1,7 +1,9 @@
 import traceback
 import types
 
-from PySide2.QtWidgets import QApplication, QMainWindow, QAction
+from PySide2.QtWidgets import (QApplication, QMainWindow, QAction, QTabWidget,
+                               QWidget, QGridLayout, QLabel, QLineEdit,
+                               QTextEdit, QPushButton)
 
 import rksmanager.database
 from . import dialogboxes
@@ -21,6 +23,9 @@ class Gui:
         main_window = QMainWindow()
         self._widgets.main_window = main_window
         self._build_menu_bar(main_window)
+        tab_holder = TabHolder()
+        self._widgets.tab_holder = tab_holder
+        main_window.setCentralWidget(tab_holder)
         main_window.show()
         qt_app.exec_()
 
@@ -71,7 +76,15 @@ class Gui:
             self._close_database()
 
         def create_person_callback():
-            pass
+            tab_id = "create_person"
+            create_person_tab = self._widgets.tab_holder.get_tab(tab_id)
+            if create_person_tab:
+                self._widgets.tab_holder.setCurrentWidget(create_person_tab)
+            else:
+                create_person_tab = PersonEditor()
+                create_person_tab.set_values({"person_id": "Not assigned yet"})
+                self._widgets.tab_holder.addTab(create_person_tab,
+                                                "Create Person", tab_id)
 
         menu_bar = window.menuBar()
         file_menu = menu_bar.addMenu("File")
@@ -119,3 +132,87 @@ class Gui:
     def _database_is_closed(self):
         self._widgets.close_db_action.setEnabled(False)
         self._widgets.people_menu.setEnabled(False)
+
+
+class TabHolder(QTabWidget):
+    def __init__(self):
+        super().__init__()
+        self.setTabsClosable(True)
+        self.tabCloseRequested.connect(self.close_tab)
+        self._tab_ids = dict()
+
+    def addTab(self, widget, label, tab_id):
+        self._tab_ids[tab_id] = widget
+        super().addTab(widget, label)
+
+    def get_tab(self, tab_id):
+        self._tab_ids.get(tab_id)
+
+    def close_tab(self, index):
+        widget = self.widget(index)
+        self.removeTab(index)
+        widget.deleteLater()
+
+
+class Label(QLabel):
+    @property
+    def value(self):
+        return self.text()
+
+    @value.setter
+    def value(self, value):
+        self.setText(value)
+
+
+class LineEdit(QLineEdit):
+    @property
+    def value(self):
+        return self.text()
+
+    @value.setter
+    def value(self, value):
+        self.setText(value)
+
+
+class TextEdit(QTextEdit):
+    @property
+    def value(self):
+        return self.toPlainText()
+
+    @value.setter
+    def value(self, value):
+        self.setPlainText(value)
+
+
+class PersonEditor(QWidget):
+    fields = (
+        ("person_id", "Person ID", Label),
+        ("first_name_or_nickname", "First name or nickname", LineEdit),
+        ("pronouns", "Pronouns", LineEdit),
+        ("notes", "Notes", TextEdit),
+    )
+
+    def __init__(self):
+        self._data_widgets = dict()
+        super().__init__()
+        layout = QGridLayout()
+        for i, field in enumerate(self.fields):
+            field_id, label, widget_type = field
+            layout.addWidget(QLabel(label), i, 0)
+            widget = widget_type()
+            layout.addWidget(widget, i, 1)
+            self._data_widgets[field_id] = widget
+        self.save_button = QPushButton("Save")
+        layout.addWidget(self.save_button, len(self.fields), 0, 1, -1)
+        self.setLayout(layout)
+
+    def get_values(self):
+        values = dict()
+        for field_id, widget in self._data_widgets.items():
+            values[field_id] = widget.value
+        return values
+
+    def set_values(self, values):
+        keys = self._data_widgets.keys() & values.keys()
+        for key in keys:
+            self._data_widgets[key].value = values[key]
